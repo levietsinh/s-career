d1224143f278f8549f5642c1efa1fe8571ecea1d
Le Viet Sinh <levietsinh201194@gmail.com>
2023-01-31 17:13:51 +0700

    [S CAREER] Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,27 @@
 import React from "react"
+
+// Styles
 import styles from "./Header.module.scss";
+
+// Images
+import logoImg from "../../assets/images/logo.svg";
+import modeImg from "../../assets/images/dark-mode.png";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      Header
+    <header className={styles["header"]}>
+      <div className={styles["header-logo"]}>
+        <img src={logoImg} alt="S Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>
+            <img className={styles["header-mode"]} src={modeImg} alt="Dark Mode" />
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
