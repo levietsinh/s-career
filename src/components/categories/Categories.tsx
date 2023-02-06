602fb8283d4d14bb00d2f4919970e848a3f7680a
Sinh Le <levietsinh201194@gmail.com>
2023-02-06 23:47:50 +0700

    [S CAREER] Carousel

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import * as React from "react";
 import styles from "./Categories.module.scss";
 import { categoriesList } from "data/categories";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import Slider, { Settings } from "react-slick";
 
 const Categories = () => {
   interface ICategory {
     name: string;
     image: string;
   }
-  const settings = {
+  const settings:Settings = {
     className: "center",
+    autoplay: true,
     centerMode: true,
     infinite: true,
     centerPadding: "60px",
