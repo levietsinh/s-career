2af0e24f33d9f07e94c301e26df45a6e498e504b
Le Viet Sinh <levietsinh201194@gmail.com>
2023-03-15 16:37:29 +0700

    Fix bugs

--- src/data/libraries/reactjs.js
+++ src/data/libraries/reactjs.js
@@ -8,9 +8,10 @@ export const data = {
   ],
   library: [
     {
-      question: "What's your name?",
-      answer:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam odio et deserunt dolores quisquam, tempore culpa dicta natus blanditiis labore recusandae vitae, repellat vero. Nulla veritatis eligendi non ab. Vel?",
+      question: "What is the Virtual DOM?",
+      answer: `
+        DOM (virtual DOM)  viết tắt của Document Object Model. DOM là một đại diện được nằm trong bộ nhớ cho một thành phần HTML thật mà cấu thành nên giao diện cho chương trình. Khi một component được thông dịch lại (re-render), DOM ảo sẽ so sánh sự thay đổi với mô hình của DOM thật để tạo một danh sách cập nhật sẽ được thực hiện. Lợi ích chính của việc này là giúp tăng hiệu năng, chỉ tập trung vào các thay đổi nhỏ và thực sự cần thiết với DOM thật hơn là phải re-render lại một tập component lớn.
+        `,
     },
     {
       question: "What's your name?",
@@ -24,15 +25,15 @@ export const data = {
     },
     {
       question: "What is the question?",
-      answer: "Updating..."
+      answer: "Updating...",
     },
     {
       question: "What is the question?",
-      answer: "Updating..."
+      answer: "Updating...",
     },
     {
       question: "What is the question?",
-      answer: "Updating..."
+      answer: "Updating...",
     },
   ],
 };
