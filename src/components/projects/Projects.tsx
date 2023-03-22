82ae849e42d66fd4559be598b0b5487d86cd0a1f
Le Viet Sinh <levietsinh201194@gmail.com>
2023-03-22 16:54:52 +0700

    [S CAREER] Add projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -5,7 +5,8 @@ import styles from "./Projects.module.scss"
 const Projects = () => {
   return (
     <div className={clsx(styles["projects"], "container")}>
-
+      <a href="https://codelearn.io/sharing/7-meo-va-thu-thuat-trong-javascript">Các tips JS nên biết</a>
+      <a href="https://topdev.vn/blog/9-du-an-moi-nhat-giup-ban-thanh-trum-front-end-trong-nam-2020/">Các dự án nên làm</a>
     </div>
   )
 }
