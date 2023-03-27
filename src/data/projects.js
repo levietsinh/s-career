e96b88f7c709fe1ee596765052b25c05233060fc
Le Viet Sinh <levietsinh201194@gmail.com>
2023-03-27 17:12:51 +0700

    a

---
+++ src/data/projects.js
@@ -0,0 +1,42 @@
+export const projects = {
+  vue: [
+    {
+      thumbnail: "",
+      title: "Title",
+      git: "git link",
+      technical: "VueJs, Bootstrap"
+    },
+    {
+      thumbnail: "",
+      title: "Title",
+      git: "git link",
+      technical: "VueJs, NuxtJs"
+    },
+    {
+      thumbnail: "",
+      title: "Title",
+      git: "git link",
+      technical: "VueJs, Bootstrap"
+    },
+  ],
+  react: [
+    {
+      thumbnail: "",
+      title: "Title",
+      git: "git link",
+      technical: "React, Bootstrap"
+    },
+    {
+      thumbnail: "",
+      title: "Title",
+      git: "git link",
+      technical: "NestJs, NuxtJs"
+    },
+    {
+      thumbnail: "",
+      title: "Title",
+      git: "git link",
+      technical: "ReactJs, Tailwind"
+    },
+  ]
+}
