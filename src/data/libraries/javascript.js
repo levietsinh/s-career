5295ca9fd048752514f35efa4b971836f94a5ab2
Sinh Le <levietsinh201194@gmail.com>
2023-04-04 23:37:54 +0700

    how does website work?

--- src/data/libraries/javascript.js
+++ src/data/libraries/javascript.js
@@ -37,6 +37,10 @@ export const data = {
       name: "The same element array",
       link: "https://jsfiddle.net/2gx913dv/1/",
     },
+    {
+      name: "A sentence is made up of a group of words. Each word is a sequence of letters, ('a'-'z', 'A'-'Z'), that may contain one or more hyphens and may end in a punctuation mark: period (.), comma (,), question mark (?), or exclamation point (!). Words will be separated by one or more white space characters. Hyphens join two words into one and should be retained while the other punctuation marks should be stripped. Determine the number of words in a given sentence. Example s = 'How many eggs are in a half-dozen, 13?' The list of words in the string is ['How', 'many', 'eggs', 'are', in', 'a', 'half-dozen'] and the number of words is 7. Notice that the numeric string, '13', is not a word because it is not within the allowed character set. Function Description Complete the function howMany in the editor below. howMany has the following parameter(s): sentence: a string Returns: int: an integer that represents the number of words in the string?",
+      link: "https://jsfiddle.net/honLakyc/",
+    },
   ],
   library: [
     {
